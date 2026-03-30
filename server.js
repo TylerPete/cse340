d738ba58e6f449881d84c4fafa78173a4909cf9c
TylerPete <tpeterson4@byupathway.edu>
2026-03-29 22:33:02 -0500

    committing all pre-CSS styling changes from Learning Activity 1 [OF 3 <:(  ] from week 3

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const env = require("dotenv").config();
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const static = require("./routes/static");
-const baseController = require("./controllers/baseController")
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -26,6 +27,9 @@ app.use(static);
 
 // Index route
 app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
