5f92a216f8378f4b6a1f57c41e96998af29e2588
TylerPete <tpeterson4@byupathway.edu>
2026-04-02 23:17:39 -0500

    fixed the stinkin flash message issue...the messages() EJS code block was literally just in the wrong spot...too far down in the index.js view from where I was looking for it to be

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(session({
 // Express Messages Middleware
 app.use(require('connect-flash')())
 app.use(function(req, res, next){
+  // res.locals.messages = require('express-messages')(req, res)
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
