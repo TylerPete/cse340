b716f89cf9426e573c373b2109d8b0b6e910b799
TylerPete <tpeterson4@byupathway.edu>
2026-04-04 09:01:52 -0500

    css styling done for registration form view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,10 @@ const router = new express.Router()
 const actController = require("../controllers/accountController")
 const utilities = require("../utilities/")
 
+// Route to build login view
 router.get("/login", utilities.handleErrors(actController.buildLogin))
+
+// Route to build registration view
+router.get("/registration", utilities.handleErrors(actController.buildRegistration))
 
 module.exports = router
