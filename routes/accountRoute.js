737826f88c7bb78c409bfaa9483c1861dcc98381
TylerPete <tpeterson4@byupathway.edu>
2026-04-05 22:29:50 -0500

    boutta test the server side validation from the learning activity (week 4)

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = new express.Router()
 const actController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require("../utilities/account-validation")
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(actController.buildLogin))
@@ -10,6 +11,10 @@ router.get("/login", utilities.handleErrors(actController.buildLogin))
 router.get("/registration", utilities.handleErrors(actController.buildRegistration))
 
 // Route to process registration form(?)
-router.post('/registration', utilities.handleErrors(actController.registerAccount))
+router.post(
+    '/registration',
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(actController.registerAccount))
 
 module.exports = router
