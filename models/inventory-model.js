7c3df86df4d9f6d39dcf7394dd67d554fddd359e
TylerPete <tpeterson4@byupathway.edu>
2026-04-07 00:56:13 -0500

    trying to the get the classification_id variable worked into every step of the MVC process so the add-inventory view's form works correctly when submitted

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -69,10 +69,10 @@ async function checkExistingClassification(classification_name) {
 /* **********************
  * Add new inventory
  * ******************** */
-async function addNewInventory(inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) {
+async function addNewInventory(classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) {
     try {
-        const sql = "INSERT INTO inventory (inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *"
-        return await pool.query(sql, [inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color])
+        const sql = "INSERT INTO inventory (classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"
+        return await pool.query(sql, [classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color])
     } catch (error) {
         return error.message
     }
